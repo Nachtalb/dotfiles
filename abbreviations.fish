d99d349002241edd49fd7ff2a0cb35eb342292f9
Nick Espig <nickespig@gmail.com>
2018-07-24 13:52:27 +0200

    Split plone-resetup into plone- resetup and start

--- abbreviations.fish
+++ abbreviations.fish
@@ -11,7 +11,8 @@ abbr pinf 'bin/instance fg'
 abbr psi 'bin/solr-instance'
 abbr pt 'bin/test'
 abbr plone-setup 'pyenv local plone-env; printf "[buildout]\nextends =\n    development.cfg\n\n[omelette]\nrecipe =\n\n" > development_nick.cfg; ln -fs development_nick.cfg buildout.cfg; python bootstrap.py; bin/buildout;'
-abbr plone-resetup 'python bootstrap.py; bin/buildout; bin/solr-instance start; bin/tika-server start 2> /dev/null &; bin/solr-instance start'
+abbr plone-resetup 'python bootstrap.py; bin/buildout'
+abbr plone-start 'bin/solr-instance start; bin/tika-server start 2> /dev/null &; bin/instance fg'
 
 # Oh my fish
 abbr omr 'omf reload'
