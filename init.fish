257a5d2f0fd667f890e15d7738d95c8792902e77
Nick Espig <nickespig@gmail.com>
2019-01-18 11:43:26 +0100

    Clean up init.fish file

--- init.fish
+++ init.fish
@@ -1,75 +1,81 @@
-# Python developers otherwise
-set -gx PYTHONDONTWRITEBYTECODE 1
-# Disable default virtualenv prompt
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
-# Load pythonrc file
-set -gx PYTHONSTARTUP $HOME/.pythonrc
+###############################################################################
+# Colours                                                                     #
+###############################################################################
 
-# Do not update on installation in homebrew
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set fish_color_autosuggestion grey
+
+
+###############################################################################
+# Set globals                                                                 #
+###############################################################################
+set -gx PYTHONDONTWRITEBYTECODE 1  # Python won’t try to write .pyc or .pyo files on the import of source modules
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1  # Disable default virtualenv prompt
+set -gx PYTHONSTARTUP $HOME/.pythonrc  # Load pythonrc file
+set -gx HOMEBREW_NO_AUTO_UPDATE 1  # Do not update on installation in homebrew
+set -gx GPG_TTY (tty)  # Load gpg
+set -gx JAVA_HOME (/usr/libexec/java_home -v 1.8)  # Set default java version
+
+# Expand $PATH
+set -gx PATH $PATH $HOME/.config/omf/bin
+set -gx PATH ~/bin/ $PATH
 
 # Lang setting
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 
+# Use vim as default editor
 set -gx VISUAL vim
 set -gx EDITOR $VISUAL
 
+# Syntax higlighting for less
 set -gx LESSOPEN "| /usr/local/bin/source-highlight-esc.sh %s"
 set -gx LESS ' -R '
-
-# Add user local ~/bin/ to path
-set -gx PATH ~/bin/ $PATH
-
-# Add etcher-cli if available
-if test -d /opt/etcher-cli
-    set -gx PATH /opt/etcher-cli $PATH
-end
-
-# Add scripts directory to PATH var
-set -gx PATH $PATH $HOME/.config/omf/bin
 
 # GH config
 set -gx GH_BASE_DIR $HOME/Development
 set -gx GL_BASE_DIR $HOME/Development
 set -gx GB_BASE_DIR $HOME/Development
 
-# Load pyenv
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
+# OpenSSL configuration
+set -gx LDFLAGS -L/usr/local/opt/openssl/lib
+set -gx CPPFLAGS -I/usr/local/opt/openssl/include
 
-# Load rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# Add etcher-cli if available
+if test -d /opt/etcher-cli
+    set -gx PATH /opt/etcher-cli $PATH
+end
 
-# Load GPG
-set -gx GPG_TTY (tty)
 
+###############################################################################
+# Load tools                                                                  #
+###############################################################################
+
+# Own configurations
 source ~/.config/omf/abbreviations.fish
 source ~/.config/omf/alias.fish
 source ~/.config/omf/hooks.fish
 
-set -gx LDFLAGS -L/usr/local/opt/openssl/lib
-set -gx CPPFLAGS -I/usr/local/opt/openssl/include
+# Pyenv
+status --is-interactive; and source (pyenv init -|psub)
+status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
-# Set default java version
-set -gx JAVA_HOME (/usr/libexec/java_home -v 1.8)
+# Rbevn
+status --is-interactive; and source (rbenv init -|psub)
 
-# Set location of tasks to OneDrive
-set -gx TASKDATA ~/OneDrive/Tasks
-
-# Load Direnv
+# direnv
 eval (direnv hook fish)
 
-# Load load untracked fish files in ./user folder
+# Git ignored files
 for filename in ~/.config/omf/user/*.fish
     source $filename
 end
 
-# Load load tracked fish files in ./scripts folder
+# Own scripts
 for file in ~/.config/omf/scripts/*.fish
     source $file
 end
 
+# Autojump
 if test -f ~/.autojump/share/autojump/autojump.fish
     source ~/.autojump/share/autojump/autojump.fish
 end
