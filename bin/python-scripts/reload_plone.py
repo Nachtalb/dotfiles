6e48a3a3e612cdba8cf690da8390479da316b546
Nick Espig <nickespig@gmail.com>
2019-01-31 14:26:47 +0100

    Add script to reload plone from the cli

---
+++ bin/python-scripts/reload_plone.py
@@ -0,0 +1,21 @@
+#!/Users/bernd/.pyenv/versions/Scripts/bin/python
+# Reload python code for plone by calling /reload?action=code
+from argparse import ArgumentParser, SUPPRESS
+
+from requests_html import HTMLSession
+
+parser = ArgumentParser('Reload Plone', add_help=False)
+
+parser.add_argument('--help', action='help', default=SUPPRESS, help='Show this help message and exit')
+parser.add_argument('-p', '--port', help='Port', default=8080)
+parser.add_argument('-h', '--host', help='Hostname', default='localhost')
+parser.add_argument('-u', '--user', help='Username', default='admin')
+parser.add_argument('-w', '--password', help='Password', default='admin')
+
+args = parser.parse_args()
+
+session = HTMLSession()
+response = session.get(f'http://{args.host}:{args.port}/reload?action=code', auth=(args.user, args.password))
+
+reload_info = response.html.xpath('//pre', first=True)
+print(reload_info.full_text.strip())
