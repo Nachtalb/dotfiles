a74cafc9a4f560e2d05d6a1af38bdad79449be71
Nick Espig <nickespig@gmail.com>
2019-05-16 13:26:39 +0200

    Add various new aliases

--- alias.fish
+++ alias.fish
@@ -1,5 +1,9 @@
 alias ranger='ranger --choosedir=$HOME/.rangerdir; cd (cat $HOME/.rangerdir)'
 alias c=clear
 
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias urldecode='python -c "import sys, urllib as ul; print(ul.unquote_plus(sys.argv[1]))"'
+alias urlencode='python -c "import sys, urllib as ul; print(ul.quote_plus(sys.argv[1])"'
+
+alias man vman
+
+alias n notify
