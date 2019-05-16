d4e7f079f3e479c6ac6d580d10e12586f7143a40
Nick Espig <nickespig@gmail.com>
2019-05-16 13:27:31 +0200

    Use vim as default pager for git

--- abbreviations.fish
+++ abbreviations.fish
@@ -61,8 +61,8 @@ abbr gsp 'git stash pop'
 abbr gsd 'git stash drop'
 # misc
 abbr glog 'git log --graph'
-abbr gl1 "git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-abbr gl2 "git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ai)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+abbr gl1 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all --color=always"
+abbr gl2 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ai)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all --color=always"
 abbr gcp 'git cherry-pick -S '
 abbr gco 'git checkout'
 abbr gcb 'git checkout -b ne/'
