c75946fe7b6d828f7429e1681c88c1bd00434059
Nick Espig <nickespig@gmail.com>
2019-08-05 14:51:15 +0200

    Move --all to end of git log abbreviations so it can be deleted faster if needed

--- abbreviations.fish
+++ abbreviations.fish
@@ -61,8 +61,8 @@ abbr gsp 'git stash pop'
 abbr gsd 'git stash drop'
 # misc
 abbr glog 'git log --graph'
-abbr gl1 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all --color=always"
-abbr gl2 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ai)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all --color=always"
+abbr gl1 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --color=always --all"
+abbr gl2 "env GIT_PAGER=less git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ai)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --color=always --all "
 abbr gcp 'git cherry-pick -S '
 abbr gco 'git checkout'
 abbr gcb 'git checkout -b ne/'
