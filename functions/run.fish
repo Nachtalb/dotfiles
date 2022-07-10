f2b316602a53918ceaff0093418aa80de121d56c
Nachtalb <info@nachtalb.io>
2022-07-10 13:13:34 +0200

    Show commands run during install

---
+++ functions/run.fish
@@ -0,0 +1,4 @@
+function run
+    set_color e91ec6 && echo "\$ $argv" & set_color normal
+    $argv
+end
--- install.fish
+++ install.fish
@@ -1,40 +1,39 @@
 #!/usr/bin/fish
 set_color green && echo "Add default directories" && set_color white
-mkdir -p ~/.ssh
-mkdir -p ~/.gnupg
-mkdir -p ~/bin
-mkdir -p ~/src/github.com
+run mkdir -p ~/.ssh
+run mkdir -p ~/.gnupg
+run mkdir -p ~/bin
+run mkdir -p ~/src/github.com
 
 set_color green && echo "Link dotfiles" && set_color white
-ln -s ~/.config/fish/_symlinks/gpg.pub ~/.gnupg/gpg.pub
-ln -s ~/.config/fish/_symlinks/id_rsa.pub ~/.ssh/id_rsa.pub
-ln -s ~/.config/fish/_symlinks/.gitconfig ~/.gitconfig
-ln -s ~/.config/fish/_symlinks/.global_gitignore ~/.global_gitignore
-ln -s ~/.config/fish/_symlinks/.pdbrc ~/.pdbrc
-ln -s ~/.config/fish/_symlinks/.pythonrc ~/.pythonrc
-ln -s ~/.config/fish/_symlinks/.tmux.conf ~/.tmux.conf
+run ln -s ~/.config/fish/_symlinks/gpg.pub ~/.gnupg/gpg.pub
+run ln -s ~/.config/fish/_symlinks/id_rsa.pub ~/.ssh/id_rsa.pub
+run ln -s ~/.config/fish/_symlinks/.gitconfig ~/.gitconfig
+run ln -s ~/.config/fish/_symlinks/.global_gitignore ~/.global_gitignore
+run ln -s ~/.config/fish/_symlinks/.pdbrc ~/.pdbrc
+run ln -s ~/.config/fish/_symlinks/.pythonrc ~/.pythonrc
+run ln -s ~/.config/fish/_symlinks/.tmux.conf ~/.tmux.conf
 
 set_color green && echo "Fix dotfile premissions" && set_color white
-chmod 0600 ~/.ssh/id_rsa
-chmod 0600 ~/.gnupg/gpg.gpg
+run chmod 0600 ~/.ssh/id_rsa
+run chmod 0600 ~/.gnupg/gpg.gpg
 
 if command -q pyenv
+    set_color green && echo "Setting up python scripts" && set_color white
     if not pyenv versions | grep -q nvim
-        set_color green && echo "Setting up python" && set_color white
-        cd ~/.vim
-        pyenv virtualenv (pyenv versions --skip-aliases --bare | rg '^3[0-9.]+$' | sort -Vr | head -n 1) nvim
-        pyenv activate nvim
-        pip install -U pip setuptools black autopep8 flake8 yapf pylint
+        run pyenv virtualenv (pyenv versions --skip-aliases --bare | rg '^3[0-9.]+$' | sort -Vr | head -n 1) nvim
+        run pyenv activate nvim
+        run pip install -U pip setuptools black autopep8 flake8 yapf pylint
     else
-        pyenv activate nvim
-        pip install -U pip setuptools black autopep8 flake8 yapf pylint
+        run pyenv activate nvim
+        run pip install -U pip setuptools black autopep8 flake8 yapf pylint
     end
-    ln -sf (pyenv which black) ~/.config/fish/bin/
-    ln -sf (pyenv which yapf) ~/.config/fish/bin/
-    ln -sf (pyenv which autopep8) ~/.config/fish/bin/
-    ln -sf (pyenv which flake8) ~/.config/fish/bin/
-    ln -sf (pyenv which pylint) ~/.config/fish/bin/
-    pyenv deactivate
+    run ln -sf (pyenv which yapf) ~/.config/fish/bin/
+    run ln -sf (pyenv which autopep8) ~/.config/fish/bin/
+    run ln -sf (pyenv which flake8) ~/.config/fish/bin/
+    run ln -sf (pyenv which pylint) ~/.config/fish/bin/
+    run pyenv deactivate
+    run ln -sf (pyenv which black) ~/.config/fish/bin/
 else
     set_color red && printf "Pyenv not installed, run: " && set_color grey && echo "curl https://pyenv.run | sh" && set_color white
 end
