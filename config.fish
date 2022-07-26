e90c3e1eafbbd47d8a26adb04f805561c4313ad1
Nachtalb <info@nachtalb.io>
2022-07-26 10:02:27 +0200

    Add functionality from garuda linux

--- config.fish
+++ config.fish
@@ -10,11 +10,29 @@ if begin; isatty; or status --is-interactive; or test -z "$INSIDE_EMACS"; or not
     end
 end
 
-# Connect to the TMUX session if it exists, or create it if it doesn't.
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    if test -f "/usr/bin/starship"
+        # Starship prompt
+        source ("/usr/bin/starship" init fish --print-full-init | psub)
+    end
+
+    if test -f /usr/share/doc/find-the-command/ftc.fish
+        # Advanced command-not-found hook for pacman
+        source /usr/share/doc/find-the-command/ftc.fish
+    end
+
 end
 
+## Set values
+# Hide welcome message
+set fish_greeting
+
+## Export variable need for qt-theme
+if type "qtile" >> /dev/null 2>&1
+   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+end
+
+## Enviromental vars
 set -gx PYTHONDONTWRITEBYTECODE 1  # Python won’t try to write .pyc or .pyo files on the import of source modules
 set -gx PYTHONUNBUFFERED 1  # Force stdin, stdout and stderr to be totally unbuffered.
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1  # Disable default virtualenv prompt
@@ -23,17 +41,24 @@ set -gx PYTHON_CONFIGURE_OPTS "--enable-shared"
 set -gx GPG_TTY (tty)  # Load gpg
 set -gx JAVA_HOME (readlink -f /usr/bin/javac | sed "s:/bin/javac::")
 set -gx GO111MODULE on
+set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+# Set settings for https://github.com/franciscolourenco/done
+set -gx __done_min_cmd_duration 10000
+set -gx __done_notification_urgency_level low
 
 set -gx VISUAL nvim
 set -gx EDITOR $VISUAL
 
-for p in /usr/bin/{chromium,microsoft-edge,microsoft-edge-dev}
+for p in /usr/bin/{chromium,microsoft-edge,microsoft-edge-beta,microsoft-edge-dev}
     if test -f $p
-        set -gx BROWSER
+        set -gx BROWSER $p
+        break
     end
 end
 
-if test (loginctl show-session (loginctl | awk '/tty/ {print $1}') -p Type | cut -d= -f2) = 'wayland'
+## WAYLAND
+if test (loginctl show-session 2 -p Type | awk -F= '{print $2}') = 'wayland'
     # Most pure GTK3 apps use wayland by default, but some,
     # like Firefox, need the backend to be explicitely selected.
     set -gx GTK_CSD 0
@@ -47,6 +72,7 @@ if test (loginctl show-session (loginctl | awk '/tty/ {print $1}') -p Type | cut
     set -gx _JAVA_AWT_WM_NONREPARENTING 1
 end
 
+## SWAY
 # set default shell and terminal
 if test -f /usr/share/sway/scripts/foot.sh
     set -gx TERMINAL_COMMAND  /usr/share/sway/scripts/foot.sh
@@ -55,9 +81,14 @@ end
 # add default location for zeit.db
 set -gx ZEIT_DB ~/.config/zeit.db
 
+# Do not show notification if terminal is focused
+set -U __done_sway_ignore_visible 1
+
+## PATH EXPANSION
 # Expand $PATH
 set -l NewPaths \
     /usr/local/go/bin \
+    $HOME/.local/bin \
     $HOME/.yarn/bin \
     $HOME/.cargo/bin \
     $HOME/.config/fish/bin \
@@ -74,6 +105,61 @@ set -gx GH_BASE_DIR $HOME/src
 set -gx GL_BASE_DIR $HOME/src
 set -gx GB_BASE_DIR $HOME/src
 
+## Useful aliases
+# Replace ls with exa
+if command -q exa
+    alias ls='exa --color=always --group-directories-first --icons' # preferred listing
+    alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+    alias la='exa -al --color=always --group-directories-first --icons'  # all files and dirs
+    alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+    alias l.="la | egrep '^\.'"                                     # show only dotfiles
+end
+alias ip="ip -color"
 
-# Do not show notification if terminal is focused
-set -U __done_sway_ignore_visible 1
+# Replace some more things with better alternatives
+if command -q bat
+    alias cat='bat --style header --style rules --style snip --style changes --style header'
+end
+
+[ ! -x /usr/bin/yay ] && [ -x /usr/bin/paru ] && alias yay='paru'
+
+if command -q pacman
+    alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+    alias rmpkg="sudo pacman -Rdd"
+
+    # Help people new to Arch
+    alias apt='man pacman'
+    alias apt-get='man pacman'
+
+    # Cleanup orphaned packages
+    alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+
+    # Recent installed packages
+    alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+end
+
+# Common use
+alias grubup="sudo update-grub"
+alias tarnow='tar -acf '
+alias untar='tar -xvf '
+alias wget='wget -c '
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias upd='/usr/bin/garuda-update'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                          # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"     # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
+
+# Get fastest mirrors
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+
+# Get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
---
+++ functions/__history_double_exclamation.fish
@@ -0,0 +1,28 @@
+# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
