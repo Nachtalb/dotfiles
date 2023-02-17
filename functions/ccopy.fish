1786c53588c295abfa7b79b15a7106b7980ad0be
Nachtalb <na@nachtalb.io>
2023-02-17 14:56:32 +0100

    Improve speed of ccopy and cpaste by a lot

--- functions/ccopy.fish
+++ functions/ccopy.fish
@@ -1,22 +1,34 @@
 function ccopy --description "Copy data from STDIN to the clipboard"
-    if command -q pbcopy
+    if set -q CCOPY
+        read -z | $CCOPY
+        return
+    else if command -q pbcopy
         read -z | pbcopy
+        set -gx "CCOPY" "pbcopy"
     else if command -q wl-copy
         read -z | wl-copy
+        set -gx "CCOPY" "wl-copy"
     else if command -q xclip
         read -z | xclip -sel clip
+        set -gx "CCOPY" "xclip" "-sel" "clip"
     else if command -q xsel
         read -z | xsel -ibs
+        set -gx "CCOPY" "xsel" "-ibs"
     else if command -q lemonade
         read -z | lemonade copy
-    else if command -q win32yank.exe
-        read -z | win32yank.exe -i -clrf
-    else if command -q termunx-clipboard-set
-        read -z | termunx-clipboard-set
+        set -gx "CCOPY" "lemonade" "copy"
     else if command -q clip.exe
         read -z | clip.exe
+        set -gx "CCOPY" "clip.exe"  # clip.exe has about double the performance of win32yank.exe
+    else if command -q win32yank.exe
+        read -z | win32yank.exe -i -clrf
+        set -gx "CCOPY" "win32yank.exe" "-i" "-clrf"
+    else if command -q termunx-clipboard-set
+        read -z | termunx-clipboard-set
+        set -gx "CCOPY" "termunx-clipboard-set"
     else if test -n $TMUX; and command -q tmux
         read -z | tmux load-buffer -
+        set -gx "CCOPY" "tmux load-buffer -"
     else
         echo "No supported clipboard util found"
     end
--- functions/cpaste.fish
+++ functions/cpaste.fish
@@ -1,22 +1,33 @@
 function cpaste --description "Print clipboard data to STOUT"
-    if command -q pbpaste
+    if set -q CPASTE
+        $CPASTE
+    else if command -q pbpaste
         pbpaste
+        set -gx CPASTE "pbpaste"
     else if command -q wl-copy
         wl-paste
+        set -gx CPASTE "wl-paste"
     else if command -q xclip
         xclip -o -sel clip
+        set -gx CPASTE "xclip" "-o" "-sel" "clip"
     else if command -q xsel
         xsel -o -b
+        set -gx CPASTE "xsel" "-o" "-b"
     else if command -q lemonade
         lemonade paste
+        set -gx CPASTE "lemonade" "paste"
     else if command -q win32yank.exe
-        win32yank.exe -o -lf
+        win32yank.exe -o --lf
+        set -gx CPASTE "win32yank.exe" "-o" "--lf"  # win32yank.exe has about double the performance of powershell.exe -c Get-Clipboard
     else if command -q termunx-clipboard-set
         termunx-clipboard-get
+        set -gx CPASTE "termunx-clipboard-set"
     else if command -q powershell.exe
-        powershell.exe -c "Get-Clipboard"
+      powershell.exe -c "Get-Clipboard"
+      set -gx CPASTE "powershell.exe" "-c" "Get-Clipboard"
     else if test -n $TMUX; and command -q tmux
         tmux save-buffer -
+        set -gx CPASTE "save-buffer" "-"
     else
         echo "No supported clipboard util found"
     end
