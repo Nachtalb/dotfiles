5728dd2359aa9db93c3745437fa675bb14aaff5d
Nachtalb <na@nachtalb.io>
2023-11-02 19:27:27 +0100

    New alias and abbriviations

--- conf.d/abbriviations.fish
+++ conf.d/abbriviations.fish
@@ -10,6 +10,10 @@ abbr grs 'git rebase --skip'
 abbr gcp 'git cherry-pick'
 abbr gcps 'git cherry-pick --abort'
 abbr gcpc 'git cherry-pick --continue'
+# git merge
+abbr gm 'git merge'
+abbr gma 'git merge --abort'
+abbr gmc 'git merge --continue'
 # git commit
 abbr ga 'git add'
 abbr gaa 'git add .'
@@ -21,38 +25,36 @@ abbr gfa 'git fetch --all -p'
 abbr gba 'env GIT_PAGER=cat git branch --all'
 abbr gbd 'git branch -D'
 abbr gbm 'git branch -m'
-# pull
+# git pull
 abbr gpl 'git pull -r'
-# reset
+# git reset
 abbr grh 'git reset --hard'
-# push
+abbr grs 'git reset --soft'
+# git push
 abbr gpf 'git push --force'
 abbr gp 'git push'
-# stashing
+# git stash
 abbr gsa 'git stash save'
 abbr gsp 'git stash pop'
 abbr gsd 'git stash drop'
 abbr gsl 'git stash list'
 abbr gss 'git stash show'
-# cherry picking
-abbr gcp 'git cherry-pick'
-abbr gcpc 'git cherry-pick --continue'
-abbr gcpa 'git cherry-pick --abort'
-# misc
+# git log
 abbr glog 'git log --graph'
 abbr gl1 "git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --color=always --all"
 abbr gl2 "git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ai)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --color=always --all "
 abbr glw "watch -n 3 --color \"git log --graph --abbrev-commit --decorate --format='%C(bold blue)%h%C(reset) - %C(bold green)(%ai)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --color=always --all\""
+# git checkout
 abbr gco 'git checkout'
 abbr gcb 'git checkout -b'
+# git diff
 abbr gst 'git status'
 abbr gdf 'git diff'
 abbr gdfn 'git diff --name-status'
 abbr gds 'git diff --staged'
 abbr gdsn 'git diff --staged --name-status'
-abbr gr 'git remote'
+# git remote
 abbr grv 'git remote -v'
 abbr gup 'git pull -r && git fetch -p origin && remove-merged-git-branches && git remote | grep -v origin | xargs -L1 git fetch -p'
 
 abbr ppy 'pyenv virtualenv (pyenv versions --skip-envs --skip-aliases --bare | sort -V | tail -n1) (basename $PWD) && pyenv local (basename $PWD) && pip install -U pip setuptools'
-abbr upip 'pip install -U pip setuptools'
--- conf.d/alias.fish
+++ conf.d/alias.fish
@@ -6,3 +6,5 @@ alias j z  # j by habbit due to autojump
 if not command -q tree
     alias tree 'exa --tree'
 end
+
+alias gr 'cd (git rev-parse --show-toplevel)'
