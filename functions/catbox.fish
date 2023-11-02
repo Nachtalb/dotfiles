c81fe6cb193f7338db1bfaa14af2ffa172a5d337
Nachtalb <na@nachtalb.io>
2023-11-02 19:42:26 +0100

    Add usage message to cat- and litterbox uploaders

--- functions/catbox.fish
+++ functions/catbox.fish
@@ -1,4 +1,9 @@
 function catbox
+  if test -z "$argv[1]"
+    echo "Usage: catbox <file>"
+    return 1
+  end
+
   curl -F "reqtype=fileupload" -F "fileToUpload=@$argv[1]" https://catbox.moe/user/api.php
 end
 
--- functions/litterbox.fish
+++ functions/litterbox.fish
@@ -1,4 +1,9 @@
 function litterbox
+  if test -z "$argv[1]"
+    echo "Usage: litterbox <file>"
+    return 1
+  end
+
   curl -F "reqtype=fileupload" -F "time=1h" -F "fileToUpload=@$argv[1]" https://litterbox.catbox.moe/resources/internals/api.php
 end
 
